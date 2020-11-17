f5e2e4d1801bc7dc970e9e5d8896299be1446c38
@kirom222 <kirom222@gmail.com>
2020-11-17 09:19:42 +0700

    update

--- components/TheSidebar.vue
+++ components/TheSidebar.vue
@@ -6,25 +6,8 @@
     @update:show="(value) => $store.commit('set', ['sidebarShow', value])"
   >
     <CSidebarBrand class="d-md-down-none" to="/">
-      <!-- <b-icon icon="exclamation-triangle"></b-icon> -->
-
-      <!-- <CIcon 
-        class="c-sidebar-brand-full" 
-        name="logo" 
-        size="custom-size" 
-        :height="35" 
-        viewBox="0 0 556 134"
-      /> -->
       <img class="c-sidebar-brand-full" src='@/assets/img/logo-kirimin-white.png'/>
       <img class="c-sidebar-brand-minimized" src='@/assets/img/logo-kirimin-min.png'/>
-
-      <!-- <CIcon 
-        class="c-sidebar-brand-minimized" 
-        name="logo" 
-        size="custom-size" 
-        :height="35" 
-        viewBox="0 0 110 134"
-      /> -->
     </CSidebarBrand>
 
     <CRenderFunction flat :content-to-render="$options.nav"/>
